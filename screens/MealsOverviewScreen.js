e96d9ec596614f2fbbce815b9312bf9ce83d9220
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-20 11:27:04 +0200

    rendering MealItem(MI) title on the MOS

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,15 +1,30 @@
-import { Text, View, StyleSheet } from "react-native";
+import { FlatList, View, StyleSheet } from "react-native";
+import { MealItem } from "../components/MealItem";
 // import { useRoute } from "@react-navigation/native"; alternative to using route prop for nested components not registered as a screen
 
 import { MEALS } from "../data/dummy-data";
 
 export const MealsOverviewScreen = ({ route }) => {
 
-  const catID = route.params.categoryId;
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem title={itemData.item.title} />
+    );
+  };
 
   return(
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catID} </Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }

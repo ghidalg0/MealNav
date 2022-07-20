3916b7d0640aca832795d2a324122c33f24f834b
Guilhem Hidalgo <guilhem.hidalgo@tetraktys-cm.com>
2022-07-20 10:44:28 +0200

    set up for RN Navigation with Expo

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { CategoriesScreen } from './screens/CategoriesScreen';
+
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
     <>
-    <SafeAreaView>
-      <StatusBar style="light" />
-      <CategoriesScreen />
-    </SafeAreaView>
+    <StatusBar style="dark" />
+    <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Meals Categories"
+            component={CategoriesScreen}
+          />
+        </Stack.Navigator>
+    </NavigationContainer>
     </>
   );
 }
